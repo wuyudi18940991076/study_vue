cdaa2da312c093c198194a56d3b32de760a222b2
e <2487136046@qq.com>
2020-02-20 14:59:52 +0800

    component2

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/view/HelloWorld.vue'
 import ocComponent from '@/view/oc_component.vue'
+import ocComponent2 from '@/view/oc_component2.vue'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/ocComponent',
       name: 'ocComponent',
       component: ocComponent
+    },
+    {
+      path: '/ocComponent2',
+      name: 'ocComponent2',
+      component: ocComponent2
     }
   ]
 })
