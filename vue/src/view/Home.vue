b20eb36f8a0dcfa8ee8f3310fd07ebb87e20d507
e <2487136046@qq.com>
2020-02-26 12:24:38 +0800

    date1

---
+++ vue/src/view/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <div id="home">
+      <div class="left">
+        <my-menu></my-menu>
+      </div>
+      <div class="right">
+        <my-header></my-header>
+      </div>
+    </div>
+    <my-date></my-date>
+  </div>
+</template>
+
+<script>
+import MyMenu from '../components/menu'
+import MyHeader from '../components/header'
+import MyDate from '../components/date'
+export default {
+  name: 'home',
+  components: { MyMenu, MyHeader, MyDate }
+}
+</script>
+<style>
+#home {
+  display: flex;
+}
+.left {
+  max-width: 280px;
+}
+.right {
+  flex: 1;
+  width: 0;
+}
+</style>
